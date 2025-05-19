72a93868ba5e0f6a9588d93a31b8f58cb9524260
artur.alves <artur.henrique@defensoria.mg.def.br>
2025-05-19 17:36:02 -0300

    fea/adicionando componente header e ajustes css

---
+++ src/components/header.tsx
@@ -0,0 +1,17 @@
+import Link from 'next/link'
+import React from 'react'
+import styles from './header.module.css'
+import Image from 'next/image'
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <nav className={`${styles.nav} container`}>
+        <Link className={styles.logo} href={'/'}>
+          <Image src={'/assets/dogs.svg'} alt='Dogs' width={28} height={22} priority />
+        </Link>
+        <Link className={styles.login} href={'/login'}>Login / Criar</Link>
+      </nav>
+    </header>
+  )
+}
