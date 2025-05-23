cf37a0c167943ed489906563000c954a9b9a2356
artur.alves <artur.henrique@defensoria.mg.def.br>
2025-05-22 22:23:01 -0300

    fea/adicionando configuracoes do prettie

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
-export default function Home() {
+import Feed from "@/components/feed/feed";
+
+export default async function Home() {
+  const response = await fetch(
+    "https://dogsapi.origamid.dev/json/api/photo/?_page=1&_total=6&_user=0"
+  );
+
   return (
     <section className="container mainContainer">
-      <h1 className="title">Projeto Dogs</h1>
+      <Feed />
     </section>
   );
 }
