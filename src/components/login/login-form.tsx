f0fdb036451d9515de25a569cd0b5b492af4c436
artur.alves <artur.henrique@defensoria.mg.def.br>
2025-07-01 22:58:41 -0300

    fea/adicionando componente input e ajustes css

--- src/components/login/login-form.tsx
+++ src/components/login/login-form.tsx
@@ -1,9 +1,14 @@
 'use client';
 
 import login from '@/actions/login';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useFormState, useFormStatus } from 'react-dom';
 import Button from '@/components/forms/button';
+import Input from '@/components/forms/input';
+import ErrorMessage from '../helper/error-message';
+import { useRouter } from 'next/navigation';
+import Link from 'next/link';
+import styles from './login-form.module.css';
 
 function FormButton() {
   const { pending } = useFormStatus();
@@ -14,20 +19,38 @@ function FormButton() {
 }
 
 export default function LoginForm() {
+  const router = useRouter();
+
   const [state, action] = useFormState(login, {
     ok: false,
     error: '',
     data: null,
   });
 
+  useEffect(() => {
+    if (state.ok) {
+      router.push('/conta');
+    }
+  }, [state.ok]);
+
   return (
     <>
-      <form action={action}>
-        <input type="text" name="username" placeholder="Usuário" />
-        <input type="password" name="password" placeholder="Senha" />
+      <form action={action} className={styles.form}>
+        <Input label="Usuário" name="username" type="text" />
+        <Input label="Senha" name="password" type="password" />
+        <ErrorMessage error={state.error} />
         <FormButton />
-        <p>{state.error}</p>
       </form>
+      <Link className={styles.esqueceu} href="/login/esqueceu-senha">
+        Esqueceu a senha?
+      </Link>
+      <div className={styles.cadastro}>
+        <h2 className={styles.subtitle}>Cadastra-se</h2>
+        <p>Ainda não possui conta? Cadastra-se no site.</p>
+        <Link className="button" href="/login/criar">
+          Cadastro
+        </Link>
+      </div>
     </>
   );
 }
