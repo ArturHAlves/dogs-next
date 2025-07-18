321945e3186b24276022637f74324266826b4551
artur.alves <artur.henrique@defensoria.mg.def.br>
2025-07-18 17:14:45 -0300

    fea/adicionando registro de usuário

--- src/app/login/criar/page.tsx
+++ src/app/login/criar/page.tsx
@@ -1,13 +1,17 @@
-import { Metadata } from 'next'
-import React from 'react'
+import LoginCriarForm from '@/components/login/login-criar-form';
+import { Metadata } from 'next';
+import React from 'react';
 
 export const metadata: Metadata = {
   title: 'Crie sua conta',
-  description: 'Crie sua conta no site ONG Dogs'
-}
+  description: 'Crie sua conta no site ONG Dogs',
+};
 
 export default function Criar() {
   return (
-    <div>Criar</div>
-  )
+    <div className="animeLeft">
+      <h1 className="title">Cadastre-se</h1>
+      <LoginCriarForm />
+    </div>
+  );
 }
