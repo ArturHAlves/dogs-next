c429487a81d1574676e17ab2ab049d8c74b7add3
artur.alves <artur.henrique@defensoria.mg.def.br>
2025-11-05 21:04:36 -0300

    chore/ajuste texto teste

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ export default async function Home() {
   return (
     <section className="container mainContainer">
       {data && <Feed photos={data} />}
+      <h1 style={{ color: '#fff' }}>Eu te amo. Ainda vou casar contigo Ana</h1>
     </section>
   );
 }
